b1efe8bd83e13ca0056a4c1acb8fa1eb1b94f795
phatphoom <neilbunner@gmail.com>
2026-07-27 01:53:46 +0700

    feat: add type for product and category

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,22 +1,3 @@
-// export interface Product {
-//   id: string;
-//   name: string;
-//   description: string;
-//   price: number;
-//   currency: string;
-//   categories: {
-//     id: number;
-//     name: string;
-//   };
-//   image_url: string;
-//   rating_rate: number;
-//   rating_count: number;
-//   in_stock: boolean;
-//   stock_count: number;
-//   discount_percentage?: number;
-//   created_at: string;
-// }
-
 // --- API Response Wrappers ---
 export interface ApiSuccessResponse<T> {
   success: true;
