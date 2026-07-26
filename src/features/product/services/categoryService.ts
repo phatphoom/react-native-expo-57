b4e36662c2a041245864bc01f337b8ad7b1993a4
phatphoom <neilbunner@gmail.com>
2026-07-27 02:02:31 +0700

    chore: remove unused files and associated references

--- src/features/product/services/categoryService.ts
+++
@@ -1,7 +0,0 @@
-import { supabase } from "@/lib/supabase";
-
-export async function getCategory() {
-  const { data, error } = await supabase.from("categories").select("*");
-  if (error) throw error;
-  return data;
-}
